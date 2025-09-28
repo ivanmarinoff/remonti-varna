fcee6f81471b38626523c6a70d0b19bb672452cd
ivanmarinoff <marinoffivan@gmail.com>
2025-09-28 16:32:11 +0300

    updated resolution

--- static/js/templatemo_script.js
+++ static/js/templatemo_script.js
@@ -2,79 +2,79 @@
 
 var menuDisabled = false;
 
-jQuery(function($) {
-    
-    $(window).load(function() { // makes sure the whole site is loaded
-            $('#status').fadeOut(); // will first fade out the loading animation
-            $('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website.
-            $('#main-wrapper').delay(350).css({'overflow':'visible'});
+jQuery(function ($) {
+
+    $(window).load(function () { // makes sure the whole site is loaded
+        $('#status').fadeOut(); // will first fade out the loading animation
+        $('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website.
+        $('#main-wrapper').delay(350).css({'overflow': 'visible'});
     });
-    
-    $(document).ready( function() {
+
+    $(document).ready(function () {
 
         loadGoogleMap();
-        
+
         // backstretch for background image
         var defaultImgSrc = $('img.main-img').attr('src');
         $.backstretch(defaultImgSrc, {speed: 500});
-	
-	// for responsive-menu
-	$("#m-btn").click(function(){
-		$("#responsive").toggle();
-	});
-	
+
+        // for responsive-menu
+        $("#m-btn").click(function () {
+            $("#responsive").toggle();
+        });
+
         // copy menu list to responsive menu
         var mainMenuList = $('#menu-list').html();
         $('#responsive').html(mainMenuList);
-	
-	//for image slide on menu item click(normal) and responsive
-	$("#menu-list a, #responsive a, .contact-menu").on('click', function(e){
-            
-			
-			if(this.className == "external") {
+
+        //for image slide on menu item click(normal) and responsive
+        $("#menu-list a, #responsive a, .contact-menu").on('click', function (e) {
+
+
+            if (this.className == "external") {
                 return;
             }
-			
-			e.preventDefault();
+
+            e.preventDefault();
 
             if (menuDisabled == false) // check the menu has disabled?
             {
                 menuDisabled = true; // disable to menu
-                
+
                 var name = $(this).attr('href');
                 $('#menu-list li').removeClass('active');
                 $('#responsive li').removeClass('active');
 
                 //  set active to both menu
                 var menuClass = $(this).parent('li').attr('class');
-                $('.'+menuClass).addClass('active');
+                $('.' + menuClass).addClass('active');
 
                 // hide responsive menu
                 $("#responsive").hide();
-                
+
                 // get image url and assign to backstretch for background
-                var imgSrc = $("img"+name+"-img").attr('src');
+                var imgSrc = $("img" + name + "-img").attr('src');
                 $.backstretch(imgSrc, {speed: 500}); //backstretch for background fade in/out
-                
+
                 // content slide in/out
-                $("section.active").animate({left:$("section.active").outerWidth()}, 400,function(){
+                $("section.active").animate({left: $("section.active").outerWidth()}, 400, function () {
                     $(this).removeClass("active");
                     $(this).hide();
-                    $(name+"-text").show();
-                    $(name+"-text").animate({left:'0px'},400,function(){
+                    $(name + "-text").show();
+                    $(name + "-text").animate({left: '0px'}, 400, function () {
                         $(this).addClass("active");
-                        
+
                         google.maps.event.trigger(map, 'resize'); // resize map
                         $.backstretch("resize"); // resize the background image
-                        
+
                         menuDisabled = false; // enable the menu
                     });
                 });
-                
+
             }
             return;
-	});
-        
+        });
+
     });
 
 });
@@ -83,13 +83,13 @@ var map = '';
 
 function initialize() {
     var mapOptions = {
-      zoom: 14,
-      center: new google.maps.LatLng(37.769725, -122.462154)
+        zoom: 14,
+        center: new google.maps.LatLng(37.769725, -122.462154)
     };
-    map = new google.maps.Map(document.getElementById('map-canvas'),  mapOptions);
+    map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 }
 
-function loadGoogleMap(){
+function loadGoogleMap() {
     // load google map
     var script = document.createElement('script');
     script.type = 'text/javascript';
@@ -97,3 +97,18 @@ function loadGoogleMap(){
         'callback=initialize';
     document.body.appendChild(script);
 }
+
+let lastScroll = 0;
+const header = document.querySelector('.affix');
+
+window.addEventListener('scroll', () => {
+    let currentScroll = window.pageYOffset;
+    if (currentScroll > lastScroll) {
+        // скролваш надолу → скрий
+        header.classList.add('header-hidden');
+    } else {
+        // скролваш нагоре → покажи
+        header.classList.remove('header-hidden');
+    }
+    lastScroll = currentScroll;
+});
